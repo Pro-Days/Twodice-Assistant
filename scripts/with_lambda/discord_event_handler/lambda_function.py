f86e28d4a74b11527891407455ae9c89f8685aaf
ProDays <joohjdays@gmail.com>
2025-02-05 04:21:02 +0900

    메시지를 모두가 볼 수 있도록 수정

--- scripts/with_lambda/discord_event_handler/lambda_function.py
+++ scripts/with_lambda/discord_event_handler/lambda_function.py
@@ -49,7 +49,9 @@ def lambda_handler(event, context):
                 "body": json.dumps(
                     {
                         "type": 5,
-                        "data": {"content": "잠시 기다려주세요", "flags": 64},
+                        "data": {
+                            "content": "잠시 기다려주세요",
+                        },
                     }
                 ),
             }
