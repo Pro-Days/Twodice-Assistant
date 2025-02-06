0960454ec460fde810291bc5f4dbe60603e5e739
ProDays <joohjdays@gmail.com>
2025-02-06 14:07:13 +0900

    어드민 커맨드 수정

--- scripts/with_lambda/main/lambda_function.py
+++ scripts/with_lambda/main/lambda_function.py
@@ -29,10 +29,32 @@ def command_handler(event):
     body = json.loads(event["body"])
     cmd = body["data"]["name"]
 
-    if (body["channel_id"] == ADMIN_CHANNEL_ID) and (body["member"]["user"]["id"] == ADMIN_ID):
-        admin(event, cmd)
+    # 어드민 커맨드
+    if body["member"]["user"]["id"] == ADMIN_ID:
+        if cmd == "ip":
+            ip = misc.get_ip()
 
-    elif cmd == "랭킹":
+            return sm.send(event, f"아이피 주소: {ip}", log_type=2)
+
+        elif cmd == "user_count":
+            pl_list = rp.get_registered_players()
+
+            return sm.send(event, f"등록된 유저 수: {len(pl_list)}", log_type=2)
+
+        elif cmd == "server_list":
+            server_list = misc.get_guild_list()
+
+            msg = (
+                f"서버 수: {len(server_list)}\n서버 목록\n"
+                + "```"
+                + ", ".join([server["name"] for server in server_list])
+                + "```"
+            )
+
+            return sm.send(event, msg, log_type=2)
+
+    # 일반 커맨드
+    if cmd == "랭킹":
         print("랭킹 command received")
         page = body["data"]["options"][0]["value"] if "options" in body["data"] else 1
         image_path = gri.get_rank_info(page)
@@ -46,29 +68,5 @@ def command_handler(event):
         return sm.send(event, msg, image=image_path)
     else:
         print("unhandled command: " + cmd)
+        sm.send(event, "오류가 발생했습니다.", log_type=3, error=f"unhandled command: {cmd}")
         return {"statusCode": 400, "body": json.dumps("unhandled command")}
-
-
-def admin(event, cmd):
-
-    if cmd == "ip":
-        ip = misc.get_ip()
-
-        return sm.send(event, f"아이피 주소: {ip}", log_type=2)
-
-    elif cmd == "user_count":
-        pl_list = rp.get_registered_players()
-
-        return sm.send(event, f"등록된 유저 수: {len(pl_list)}", log_type=2)
-
-    elif cmd == "server_list":
-        server_list = misc.get_guild_list()
-
-        msg = (
-            f"서버 수: {len(server_list)}\n서버 목록\n"
-            + "```"
-            + ", ".join([server["name"] for server in server_list])
-            + "```"
-        )
-
-        return sm.send(event, msg, log_type=2)
